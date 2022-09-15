43aad73e6810d64392abf0d4b856e505bade7001
JulianZgheib <zgheibjulien0@gmail.com>
2022-09-15 16:46:25 +0400

    include cart to Project urls.py

--- Pinchi/urls.py
+++ Pinchi/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from groceries import views
+from cart import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('groceries.urls'))
+    path('',include('groceries.urls')),
+    path('',include('cart.urls'))
 ]
